157bbf6255a30104d914eb52de5f7da3e223f6e8
SolidSnake <abdelmounaimrihan@gmail.com>
2025-02-04 19:10:00 +0100

    est

--- app/plans/page.js
+++ app/plans/page.js
@@ -115,6 +115,7 @@ const page = () => {
               Get started
             </a>
           </div>
+          {/* test */}
 
           <div class="flex flex-col max-w-lg p-6 mx-auto text-center text-gray-900 bg-white border border-gray-100 rounded-lg shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
             <h3 class="mb-4 text-2xl font-semibold">Ultimate </h3>
